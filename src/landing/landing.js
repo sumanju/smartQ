ca8bbe1dde979d2fe0c150234e5aca15523a4e16
sumanmandal <suman.mandal@obopay.com>
2021-01-07 20:52:15 +0530

    test

---
+++ src/landing/landing.js
@@ -0,0 +1,118 @@
+import React from 'react'
+import './landing.css';
+
+class Landing extends React.Component {
+
+  listOfShops = []
+
+  constructor(props) {
+    super(props)
+
+    this.state  = {
+      shopItemArr : []
+    }
+    
+    for(let pr in this.props.data.menuDetails)  {
+      this.listOfShops.push(pr)
+    }
+  }
+
+  componentDidMount() {
+    if (this.listOfShops.length)  {
+      this.selectShop(this.listOfShops[0])
+    }
+  }
+
+  selectShop(data)  {
+    
+    this.setState({
+      shopItemArr  :  this.props.data.menuDetails[data]
+    })
+  }
+
+
+
+  isRange(startTime, endTime, currTime)  {
+
+    const [hrsStart , minStart] = startTime.split(':'),
+          [hrsEnd   , minEnd]   = endTime.split(':')
+
+
+    let start = new Date(),
+        end = new Date()
+
+    start.setHours(+hrsStart)
+    start.setMinutes(+minStart)
+    start.setSeconds(0)
+
+    end.setHours(+hrsEnd)
+    end.setMinutes(+minEnd)
+    end.setSeconds(0)
+
+    if (currTime.getTime() >= start.getTime() && currTime.getTime() <=end.getTime())  {
+      return true
+    }
+
+    return false
+  }
+
+  IsItemAvailable(dateStamp) {
+    const currTime  = new Date(),
+          setOfTimeSpan = dateStamp.split(',')
+
+    for (let i = 0; i < setOfTimeSpan.length; i++)  {
+      const [startTime, endTime]  = setOfTimeSpan[i].split('-')
+
+      if (this.isRange(startTime, endTime, currTime))  {
+        return true
+      }
+    }
+    return false
+  }
+
+
+
+  render()  {
+
+    return (
+      <div className="App">
+        {/* header */}
+        <div  className="header">
+
+        </div>
+
+        {/* body */}
+        <div  className="body">
+
+        {/* side navbar */}
+          <div className="nav-bar">
+            {
+              this.listOfShops.map(data => <div onClick={() => this.selectShop(data)} key={data} className="nav-bar-menu"> {data} </div> )
+            }
+          </div>
+          
+          <div className="menu-list">
+            {
+              this.state.shopItemArr.map(data => data.ofs || !this.IsItemAvailable(data.servingtime) 
+                                                            ? <div className="menu-item-box not-available"> {data.foodname} </div>             
+                                                            : <div className="menu-item-box available"> {data.foodname} </div>  
+              )
+            }
+          </div>
+  
+        </div>
+
+        <footer className="footer">
+          <button className="button"> ALL UNAVAILABLE</button>
+          <button className="button"> ALL AVAILABLE</button>
+        </footer>
+       
+      </div>
+    )
+
+  }
+ 
+}
+
+export default Landing;
+
